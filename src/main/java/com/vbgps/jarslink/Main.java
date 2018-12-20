d64420905c0f9a74a8e6843fa4dee4c810ba1ad3
yebing <yebing@vvsmark.com>
2018-12-20 12:48:29 +0800

    update

--- src/main/java/com/vbgps/jarslink/Main.java
+++ src/main/java/com/vbgps/jarslink/Main.java
@@ -1,11 +1,41 @@
 package com.vbgps.jarslink;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.alipay.jarslink.api.Module;
+import com.alipay.jarslink.api.ModuleManager;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new FileSystemXmlApplicationContext("classpath:spring-context.xml");
+		System.out.println("classloader="+Main.class.getClassLoader());
+		InputStream is = System.in;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		try {
+			while (true) {
+				String line = reader.readLine();
+				if ("list".equals(line)) {
+					ModuleManager moduleManager = (ModuleManager) applicationContext.getBean("moduleManager");
+					List<Module> modules = moduleManager.getModules();
+					if (modules.isEmpty()) {
+						System.out.println("没有加载的模块列表");
+					}
+					for (Module module : modules) {
+						System.out.println(module.getName() + ":" + module.getVersion());
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/vbgps/jarslink/ModuleRefreshSchedulerImpl.java
+++ src/main/java/com/vbgps/jarslink/ModuleRefreshSchedulerImpl.java
@@ -1,6 +1,8 @@
 package com.vbgps.jarslink;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.alipay.jarslink.api.ModuleConfig;
@@ -12,17 +14,32 @@ public class ModuleRefreshSchedulerImpl extends AbstractModuleRefreshScheduler {
 
 	@Override
 	public List<ModuleConfig> queryModuleConfigs() {
-		return ImmutableList.of(buildModuleConfig());
+		return buildModuleConfig();
 	}
 
-	public static ModuleConfig buildModuleConfig() {
-        URL demoModule = Thread.currentThread().getContextClassLoader().getResource("META-INF/spring/demo-1.0.0.jar");
-        ModuleConfig moduleConfig = new ModuleConfig();
-        moduleConfig.setName("demo");
-        moduleConfig.setEnabled(true);
-        moduleConfig.setVersion("1.0.0.20170621");
-        moduleConfig.setProperties(ImmutableMap.of("svnPath", new Object()));
-        moduleConfig.setModuleUrl(ImmutableList.of(demoModule));
-        return moduleConfig;
-    }
+	public static List<ModuleConfig> buildModuleConfig() {
+		List<ModuleConfig> moduleConfigList = new ArrayList<ModuleConfig>();
+		URL root = ModuleRefreshSchedulerImpl.class.getResource("/");
+		String libFolderPath = root.getPath() + "libs/";
+		System.out.println(libFolderPath);
+		File libFolder = new File(libFolderPath);
+		if (!libFolder.exists()) {
+			libFolder.mkdir();
+		}
+		File[] files = libFolder.listFiles();
+		for (File file : files) {
+			if (!file.getName().endsWith(".jar")) {
+				continue;
+			}
+			URL demoModule = ModuleRefreshSchedulerImpl.class.getResource("/libs/" + file.getName());
+			ModuleConfig moduleConfig = new ModuleConfig();
+			moduleConfig.setName("demo");
+			moduleConfig.setEnabled(true);
+			moduleConfig.setVersion("1.0.0.20170621");
+			moduleConfig.setProperties(ImmutableMap.of("svnPath", new Object()));
+			moduleConfig.setModuleUrl(ImmutableList.of(demoModule));
+			moduleConfigList.add(moduleConfig);
+		}
+		return moduleConfigList;
+	}
 }
